28b9f8541f2b13587a916539eff15c8d5b5bb74b
judson <fariaas.almeida@gmail.com>
2026-08-04 08:52:04 -0300

    adiciona codigo completo

--- src/main/java/Teste/exercicio.java
+++ src/main/java/Teste/exercicio.java
@@ -1,4 +1,23 @@
 package Teste;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class exercicio {
+
+    String product1 ="computer";
+    String product2 = "office desk";
+
+    int age = 30;
+    int code = 5290;
+    char gender = 'f';
+
+    double price1 = 2100.0;
+    double price2 = 650.50;
+    double measure = 53.234567;
+
+    public static void main(String[] args){
+
+        System.out.println("product1");
+    }
+
 }
--- src/main/java/Teste/teste.java
+++ src/main/java/Teste/teste.java
@@ -1,4 +1,28 @@
 package Teste;
 
 public class teste {
+
+
+    public static void main(String[] args) {
+
+        double x, y;
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
