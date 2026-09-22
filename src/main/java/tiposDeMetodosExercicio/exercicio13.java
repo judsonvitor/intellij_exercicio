cd5e689cd6ce7e5b373bfb9581b71ba56c88ccb0
ti.menor <fariaas.almeida@gmail.com>
2026-09-22 11:35:38 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/tiposDeMetodosExercicio/exercicio13.java
@@ -0,0 +1,38 @@
+///
+package tiposDeMetodosExercicio;
+
+import java.util.Scanner;
+
+class exercicioo13 {
+
+        static int lerDados() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Digite sua idade: ");
+            int idade = Integer.parseInt(scanner.nextLine());
+            return idade;
+        }
+
+        static boolean processar(int idade) {
+            if (idade >= 18) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    static void apresentaDados(boolean resultado) {
+    if (resultado) {
+        System.out.println("Voce é maior de idade.");
+    } else {
+        System.out.println("Você é menor de idade");
+    }
+
+
+    }
+
+    public static void main() {
+        int idadeinformada = lerDados();
+        boolean ehmaiordeidade = apresentaDados(idadeinformada);
+        apresentaDados(ehmaiordeidade);
+    }
+}
